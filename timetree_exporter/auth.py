94f817eb284d0c1ec2a14ac023b1dca187521abb
蔡鳳駿 <eoleedimin@gmail.com>
2024-11-15 09:18:28 +0800

    feat: automate login and fetch

---
+++ timetree_exporter/auth.py
@@ -0,0 +1,47 @@
+"""
+This module contains the User class, which is responsible for handling user-related operations.
+"""
+
+import uuid
+import logging
+import requests
+from timetree_exporter.const import API_BASEURI, API_USER_AGENT
+
+logger = logging.getLogger(__name__)
+
+
+def login(email, password) -> str | None:
+    """
+    Log in to the TimeTree app and return the session ID.
+    """
+    session = requests.Session()
+    url = "https://timetreeapp.com/signin"
+    response = session.get(url)
+
+    url = f"{API_BASEURI}/auth/email/signin"
+    payload = {
+        "uid": email,
+        "password": password,
+        "uuid": str(uuid.uuid4()).replace("-", ""),
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "X-Timetreea": API_USER_AGENT,
+    }
+    response = session.put(url, json=payload, headers=headers)
+
+    if response.status_code != 200:
+        logger.error("Login failed: %s", response.text)
+        raise AuthenticationError("Login failed")
+
+    try:
+        session_id = response.cookies["_session_id"]
+        return session_id
+    except KeyError:
+        return None
+
+
+class AuthenticationError(Exception):
+    """
+    Exception raised when the user is not authorized to access the resource.
+    """
