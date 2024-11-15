8c076e4426cf419a0ffb71d1bce41542cbfa695e
蔡鳳駿 <eoleedimin@gmail.com>
2024-11-15 09:18:28 +0800

    feat: automate login and fetch

---
+++ timetree_exporter/calendar.py
@@ -0,0 +1,80 @@
+"""
+Timetree calendar API
+"""
+
+import logging
+import requests
+from requests.exceptions import HTTPError
+from timetree_exporter.const import API_BASEURI, API_USER_AGENT
+
+logger = logging.getLogger(__name__)
+
+
+class TimeTreeCalendar:
+    """
+    Timetree calendar API
+    """
+
+    def __init__(self, session_id: str):
+        self.session = requests.Session()
+        self.session.cookies.set("_session_id", session_id)
+
+    def get_metadata(self):
+        """
+        Get calendar metadata.
+        """
+        url = f"{API_BASEURI}/calendars?since=0"
+        response = self.session.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "X-Timetreea": API_USER_AGENT,
+            },
+        )
+        if response.status_code != 200:
+            logger.error(response.text)
+            raise HTTPError("Failed to get calendar metadata")
+        return response.json()["calendars"]
+
+    def get_events_recur(self, calendar_id: int, since: int):
+        """
+        Get events from the calendar.(Recursively)
+        """
+        url = f"{API_BASEURI}/calendar/{calendar_id}/events/sync?since={since}"
+        response = self.session.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "X-Timetreea": API_USER_AGENT,
+            },
+        )
+
+        r_json = response.json()
+
+        events = r_json["events"]
+        if r_json["chunk"] is True:
+            events.extend(self.get_events_recur(calendar_id, r_json["since"]))
+        return events
+
+    def get_events(self, calendar_id: int, calendar_name: str = None):
+        """
+        Get events from the calendar.
+        """
+        url = f"{API_BASEURI}/calendar/{calendar_id}/events/sync"
+        response = self.session.get(
+            url,
+            headers={"Content-Type": "application/json", "X-Timetreea": API_USER_AGENT},
+        )
+        if response.status_code != 200:
+            if calendar_name is not None:
+                logger.error("Failed to get events of the calendar '%s'", calendar_name)
+            else:
+                logger.error("Failed to get events of the calendar")
+            logger.error(response.text)
+
+        r_json = response.json()
+        events = r_json["events"]
+        if r_json["chunk"] is True:
+            events.extend(self.get_events_recur(calendar_id, r_json["since"]))
+
+        return events
